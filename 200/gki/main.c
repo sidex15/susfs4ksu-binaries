e3361f65f8d7ed92954c45df4dd8b26b2281e30d
sidex15 <24408329+sidex15@users.noreply.github.com>
2026-01-20 23:34:52 +0800

    v2.0.0: Refined and renamed hide_sus_mnts_for_all_procs to hide_sus_mnts_for_non_su_procs

--- 200/gki/main.c
+++ 200/gki/main.c
@@ -28,7 +28,7 @@
 #define CMD_SUSFS_SET_SDCARD_ROOT_PATH 0x55552
 #define CMD_SUSFS_ADD_SUS_PATH_LOOP 0x55553
 #define CMD_SUSFS_ADD_SUS_MOUNT 0x55560
-#define CMD_SUSFS_HIDE_SUS_MNTS_FOR_ALL_PROCS 0x55561
+#define CMD_SUSFS_HIDE_SUS_MNTS_FOR_NON_SU_PROCS 0x55561
 #define CMD_SUSFS_UMOUNT_FOR_ZYGOTE_ISO_SERVICE 0x55562
 #define CMD_SUSFS_ADD_SUS_KSTAT 0x55570
 #define CMD_SUSFS_UPDATE_SUS_KSTAT 0x55571
@@ -100,7 +100,7 @@ struct st_susfs_sus_mount {
 	int                     err;
 };
 
-struct st_susfs_hide_sus_mnts_for_all_procs {
+struct st_susfs_hide_sus_mnts_for_non_su_procs {
 	bool                    enabled;
 	int                     err;
 };
@@ -259,13 +259,12 @@ static void print_help(void) {
 	log("      |--> Added mounted path will be hidden from /proc/self/[mounts|mountinfo|mountstats]\n");
 	log("      |--> Please be reminded that the target path must be added after the bind mount or overlay operation, otherwise it won't be effective\n");
 	log("\n");
-	log("    hide_sus_mnts_for_all_procs <0|1>\n");
-	log("      |--> 0 -> Do not hide sus mounts for all processes but only non ksu process\n");
-	log("      |--> 1 -> Hide all sus mounts for all processes no matter they are ksu processes or not\n");
+	log("    hide_sus_mnts_for_non_su_procs <0|1>\n");
+	log("      |--> 0 -> DO NOT hide sus mounts for non-su processes\n");
+	log("      |--> 1 -> Hide all sus mounts for non-su processes\n");
 	log("      |--> NOTE:\n");
-	log("           - It is set to 1 in kernel by default\n");
-	log("           - It is recommended to set to 0 after screen is unlocked, or during service.sh or boot-completed.sh stage, as this should fix the issue on some rooted apps that rely on mounts mounted by ksu process\n");
-	log("\n");
+	log("           - It is set to 0 in kernel by default\n");
+	log("           - For ReZygisk without TreatWheel module, it is recommended to set to 1 in post-fs-data.sh to prevent zygote from caching the sus mounts in memory, and revert to 0 in boot-completed.sh stage, or keep it enabled if you want to keep them hidden from /proc/self/[mounts|mountinfo|mountstat] for non-su processes\n");
 	log("    umount_for_zygote_iso_service <0|1>\n");
 	log("      |--> 0 -> Do not umount for zygote spawned isolated service process\n");
 	log("      |--> 1 -> Enable to umount for zygote spawned isolated service process\n");
@@ -414,9 +413,9 @@ int main(int argc, char *argv[]) {
 		syscall(SYS_reboot, KSU_INSTALL_MAGIC1, SUSFS_MAGIC, CMD_SUSFS_ADD_SUS_MOUNT, &info);
 		PRT_MSG_IF_CMD_NOT_SUPPORTED(info.err, CMD_SUSFS_ADD_SUS_MOUNT);
 		return info.err;
-	// hide_sus_mnts_for_all_procs
-	} else if (argc == 3 && !strcmp(argv[1], "hide_sus_mnts_for_all_procs")) {
-		struct st_susfs_hide_sus_mnts_for_all_procs info = {0};
+	// hide_sus_mnts_for_non_su_procs
+	} else if (argc == 3 && !strcmp(argv[1], "hide_sus_mnts_for_non_su_procs")) {
+		struct st_susfs_hide_sus_mnts_for_non_su_procs info = {0};
 
 		if (strcmp(argv[2], "0") && strcmp(argv[2], "1")) {
 			print_help();
@@ -424,8 +423,8 @@ int main(int argc, char *argv[]) {
 		}
 		info.enabled = atoi(argv[2]);
 		info.err = ERR_CMD_NOT_SUPPORTED;
-		syscall(SYS_reboot, KSU_INSTALL_MAGIC1, SUSFS_MAGIC, CMD_SUSFS_HIDE_SUS_MNTS_FOR_ALL_PROCS, &info);
-		PRT_MSG_IF_CMD_NOT_SUPPORTED(info.err, CMD_SUSFS_HIDE_SUS_MNTS_FOR_ALL_PROCS);
+		syscall(SYS_reboot, KSU_INSTALL_MAGIC1, SUSFS_MAGIC, CMD_SUSFS_HIDE_SUS_MNTS_FOR_NON_SU_PROCS, &info);
+		PRT_MSG_IF_CMD_NOT_SUPPORTED(info.err, CMD_SUSFS_HIDE_SUS_MNTS_FOR_NON_SU_PROCS);
 		return info.err;
 	// umount_for_zygote_iso_service
 	} else if (argc == 3 && !strcmp(argv[1], "umount_for_zygote_iso_service")) {
